e6609269bdf1741ce8fda21b30f2761d0279cae4
abdelmounaim <abdelmounaim.chnafi@frencht.co>
2019-09-13 19:30:09 +0200

    test

---
+++ FrenchT/okyoto1/registration.php
@@ -0,0 +1,10 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+
+    'frontend/frencht/okyoto',
+
+    __DIR__
+);
